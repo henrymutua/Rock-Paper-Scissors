e6f28ada88c5cc276551fd5053fc5ebf995845cb
henry mutua <mutuahenry116@gmail.com>
2023-01-21 22:03:25 +0300

    created a single round of the game

---
+++ rockpaperscissors.js
@@ -0,0 +1,29 @@
+const getComputerChoice = () =>{
+    let someArray = ['rock', 'paper', 'scissors'];
+    return someArray[Math.floor(Math.random()*someArray.length)];
+}
+
+const playRound = (playerSelection, computerSelection) => {
+    computerSelection = getComputerChoice();
+   playerSelection = prompt('Please enter something!')
+if (playerSelection == 'rock' && computerSelection == 'scissors'){
+    return('You win! rock beats scissors!')
+}
+else if(playerSelection == 'rock' && computerSelection == 'paper'){
+    return('You lose. Paper beats rock!')
+}
+else if(playerSelection == 'scissors' && computerSelection == 'rock'){
+    return('You lose! Rock beats scissors')
+}
+else if(playerSelection == 'paper' && computerSelection == 'rock'){
+    return('You win! Paper beats rock')
+}
+else if(playerSelection == 'paper' && computerSelection == 'scissors'){
+    return('You lose! Scissors beats paper!')
+}
+else if(playerSelection == 'scissors' && computerSelection == 'paper'){
+    return('You win! Scissors beats paper')
+}
+}
+const savePlayRound = playRound();
+console.log(savePlayRound);
