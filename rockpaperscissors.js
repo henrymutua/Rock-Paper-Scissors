49608e334a7e95037f52878bea3908882c2e47df
henry mutua <mutuahenry116@gmail.com>
2023-01-22 17:43:15 +0300

    Final version

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -27,3 +27,8 @@ else if(playerSelection == 'scissors' && computerSelection == 'paper'){
 }
 const savePlayRound = playRound();
 console.log(savePlayRound);
+const game = () => {
+    for( let i=1; i<=5; i++){
+        return(playRound())
+    }
+}
